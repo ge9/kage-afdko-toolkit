8f57503767180bfa8d1bbf26a4cfcf6e2e511fca
k <kensho.tsurusaki@gmail.com>
2024-12-03 10:05:52 +0900

    update document

--- glyphs/make-aj1.py
+++ glyphs/make-aj1.py
@@ -1,5 +1,9 @@
-# 現在は漢字CID一覧から（aj1-xxxxx）ではなくAJ1のSequenceファイルをもとに直接IVSグリフ（uXXXX-ueXXXX）を取っているので、このスクリプトは実質的には不要
+# [CID -> GlyphWiki実体のマップ (ただし同一CIDが複数回含まれてもよいが、その場合は後に書いたものが優先される) ] と [AJ1のSequenceファイル] が与えられると、
+# *.glyphsファイルの書式（つまり、「GlyphWiki実体 + そのCIDを使用する文字の一覧 + CID」のリスト）で出力される。
+# CIDを維持するため、実体が重複していても統合はせずそのまま別々に出力される。
+# 従来はaj1-XXXXXグリフを使用していたが、現在はAJ1のSequenceファイルをもとに直接IVSグリフ（uXXXX-ueXXXX）を取っているので、このスクリプトは若干二度手間感がある
 import sys
+# GlyphWiki実体 -> (それを使用するCID, それを使用するグリフ一覧)　のマップ
 dic = {}
 # CID -> GlyphWiki実体のマップ
 with open(sys.argv[1]) as f1:
@@ -15,5 +19,5 @@ with open(sys.argv[2]) as f:
             s = line.rstrip().split(";")
             dic[s[2][5:]][1].append(s[0])
 for d in dic.items():
-    dd = "<"+">,<".join(d[1][1])+">"
+    dd = "<" + ">,<".join(d[1][1]) + ">"
     print("gw/"+d[1][0]+'\t'+dd+"\t"+d[0])
